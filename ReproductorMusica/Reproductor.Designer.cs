e4258cad22276cdc2ca20d4855646308bbf116e4
Gil Cruz <acruzantony@gmail.com>
2022-12-19 23:43:29 -0600

    Aplicacion minireproductor, windows form en c#

---
+++ ReproductorMusica/Reproductor.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ReproductorMusica
+{
+    partial class Reproductor
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Reproductor));
+            this.WinMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.AbrirMp3 = new System.Windows.Forms.OpenFileDialog();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.Historial = new System.Windows.Forms.RichTextBox();
+            this.btnCrearLista = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.WinMediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WinMediaPlayer
+            // 
+            this.WinMediaPlayer.AllowDrop = true;
+            this.WinMediaPlayer.Enabled = true;
+            this.WinMediaPlayer.Location = new System.Drawing.Point(12, 0);
+            this.WinMediaPlayer.Name = "WinMediaPlayer";
+            this.WinMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("WinMediaPlayer.OcxState")));
+            this.WinMediaPlayer.Size = new System.Drawing.Size(413, 248);
+            this.WinMediaPlayer.TabIndex = 0;
+            this.WinMediaPlayer.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.WinMediaPlayer_PlayStateChange);
+            // 
+            // AbrirMp3
+            // 
+            this.AbrirMp3.FileName = "openFileDialog1";
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(350, 182);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(75, 23);
+            this.btnCargar.TabIndex = 1;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // Historial
+            // 
+            this.Historial.Location = new System.Drawing.Point(12, 254);
+            this.Historial.Name = "Historial";
+            this.Historial.Size = new System.Drawing.Size(413, 157);
+            this.Historial.TabIndex = 2;
+            this.Historial.Text = "";
+            // 
+            // btnCrearLista
+            // 
+            this.btnCrearLista.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnCrearLista.Location = new System.Drawing.Point(724, 125);
+            this.btnCrearLista.Name = "btnCrearLista";
+            this.btnCrearLista.Size = new System.Drawing.Size(75, 23);
+            this.btnCrearLista.TabIndex = 3;
+            this.btnCrearLista.Text = "Crear Lista";
+            this.btnCrearLista.UseVisualStyleBackColor = false;
+            this.btnCrearLista.Click += new System.EventHandler(this.btnCrearLista_Click);
+            // 
+            // Reproductor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 504);
+            this.Controls.Add(this.btnCrearLista);
+            this.Controls.Add(this.Historial);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.WinMediaPlayer);
+            this.Name = "Reproductor";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.WinMediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer WinMediaPlayer;
+        private System.Windows.Forms.OpenFileDialog AbrirMp3;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.RichTextBox Historial;
+        private System.Windows.Forms.Button btnCrearLista;
+    }
+}
+
---
+++ ReproductorMusica/Reproductor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ReproductorMusica
+{
+    public partial class Reproductor : Form
+    {
+        string[] paylist;
+        int numPista=0;
+        public Reproductor()
+        {
+            InitializeComponent();
+            WinMediaPlayer.URL = @"C:\Users\OWNER\Music\Musica\ACDC\Disc 1\01.Thunderstruck.mp3";
+            WinMediaPlayer.enableContextMenu = false;
+            
+           
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            
+         
+            AbrirMp3.Multiselect = true;
+            AbrirMp3.Filter = "Archivos de Audio(*.mp3;*.wpl;*.wma)|*.mp3;*.wpl;*.wma";
+            AbrirMp3.CheckFileExists = true;
+            AbrirMp3.Title = "Abrir Archivo";
+            AbrirMp3.ShowDialog(this);
+            try
+            {
+                AbrirMp3.OpenFile();
+                paylist = AbrirMp3.FileNames;
+                WinMediaPlayer.URL = paylist[numPista];
+                foreach(string archivos in paylist)
+                {
+                    Historial.Text += archivos + "\n";
+                }
+            }
+            catch { }
+          
+
+            
+        }
+
+        private void WinMediaPlayer_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            
+            Historial.Text+=(WinMediaPlayer.playState.ToString())+"\n";
+            try
+            {
+                if ((WinMediaPlayer.playState.ToString()).Equals("wmppsMediaEnded") && paylist.Length > 0 && numPista <= paylist.Length)
+                {
+                    numPista++;
+                    WinMediaPlayer.URL = paylist[numPista];
+                    Historial.Text += paylist[numPista] + "\n";
+                    
+                }
+                if ((WinMediaPlayer.playState.ToString()).Equals("wmppsReady"))
+                {
+                    WinMediaPlayer.Ctlcontrols.play();
+                }
+            }
+            catch { }
+        }
+
+        private void axVLCPlugin21_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCrearLista_Click(object sender, EventArgs e)
+        {
+            XmlTextWriter writer;
+            writer = new XmlTextWriter("Lista.wpl", Encoding.UTF8);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartDocument();
+            writer.WriteStartElement("smil");
+            writer.WriteElementString("nodo1", "texto del nodo1");
+            writer.WriteElementString("nodo2", "texto del nodo2");
+
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Flush();
+            writer.Close();
+        }
+    }
+}
